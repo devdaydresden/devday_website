0e2c081fa2df8ceccc98ff4f3e43377c1e9417b9
Felix Herzog <felix.herzog@t-systems.com>
2016-01-26 13:51:16 +0100

    added some speakers

---
+++ contents/speaker/thomas-haase.php
@@ -0,0 +1,17 @@
+<?php
+    $name               = "Thomas Haase";
+    $imgURL             = "./contents/speaker/images/thomashaase.jpg";
+    $jobDescription     = "Security Tester";
+    $speakerText        = "<p>Thomas Haase - Penetrationstester in den Bereichen Web-, Mobile- und Embedded Security und seit 2004 Projektfeldmanager "Data Privacy and Security" bei T-Systems Multimedia Solutions GmbH.</p>
+							<br>
+							<p>Seine Schwerpunkte sind Infrastructure and Application Security, Penetrations Tests und Source Code Analysen. Außerdem engagiert er sich als Dozent für verschiedene Bildungseinrichtungen im Bereich Security Awareness und Hacking. Er ist zertifiziert als ISO27001 Lead Auditor, Certified Ethical Hacker und TeleTrusT Information Security Professional (TISP).</p>";
+    $speakerSessionText = "<p>Im Rahmen der zunehmenden Digitalisierung und der einhergehenden Vernetzung geraten immer mehr kritische Infrastrukturen und die vernetzten Geräte in den Fokus krimineller Hacker. Früher waren die meisten viele dieser Netze von den öffentlichen Netzen / Internet strikt getrennt und von außen nicht erreichbar. Doch auch der Einzug digitaler Technologien und die damit einhergehende enge Integration externer Geschäftspartner und Kunden in die Wertschöpfungskette bergen für die Unternehmen neue Gefahren. Allerdings werden immer mehr Firmen im Rahmen des sogenannten „digitalen Darwinismus“ in eine Situation gebracht, wo sie gezwungen sind, etwaige Bedenken zu ignorieren.</p>
+							<br>
+							<p>Im Rahmen des Vortrages werden mögliche Gefahren und Probleme anhand des Tests der Infrastruktur eines Fußballstadions und der darin eingebundenen Geräte demonstriert. Anhand von Praxisbeispielen werden Schwachstellen / Programmierfehler bei der Entwicklung solcher Systeme aufgezeigt und mögliche Verbesserungsmaßnahmen diskutiert.</p>";
+    $fbLink             = "";
+    $twLink             = "";
+    $googleLink         = "";
+    $linkedinLink       = "";
+?>
+
+<?php include 'template/speaker-template.php' ?>

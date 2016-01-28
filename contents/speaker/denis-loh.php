8e63112b16ddd8729656d228b020a13ff21fe221
Felix Herzog <felix.herzog@t-systems.com>
2016-01-28 16:29:58 +0100

    finally edited speakers and edited session plan

---
+++ contents/speaker/denis-loh.php
@@ -0,0 +1,13 @@
+<?php
+    $name               = "Denis Loh";
+    $imgURL             = "./contents/speaker/images/Denis_Loh.jpg";
+    $jobDescription     = "Software Architect";
+    $speakerText        = "<p>Denis Loh ist seit über zwei Jahren Softwarearchitekt in der T-Systems Multimedia Solutions GmbH und hat drei Jahre Erfahrung in der Entwicklung mobiler Apps für Android und iOS. Er beschäftigt sich sowohl beruflich als auch privat mit aktuellen Gadgets und technischen Herausforderungen rund um Beacons, Virtual Reality, Augmented Reality und IoT.</p>";
+    $speakerSessionText = "<p></p>";
+    $fbLink             = "";
+    $twLink             = "";
+    $googleLink         = "";
+    $linkedinLink       = "";
+?>
+
+<?php include 'template/speaker-template.php' ?>
--- contents/sponsors.php
+++ contents/sponsors.php
@@ -9,18 +9,18 @@
 
         <ul class="center list-inline">
             <li>
-                <a href="#"><img src="images/tsystems.png" alt=""></a>
+                <a href="https://t-systems-mms.com"><img src="images/tsystems.png" alt="T-Systems"></a>
             </li>
             <li>
-                <a href="#"><img src="images/lineupr.png" alt=""></a>
-            </li>
-            <!-- li>
-                <a href="#"><img src="images/google.png" alt=""></a>
+                <a href="https://lineupr.com"><img src="images/lineupr.png" alt="LINEUPR"></a>
             </li>
             <li>
-                <a href="#"><img src="images/focus.png" alt=""></a>
+                <a href="https://devopenspace.de"><img src="images/DOS-Logo-56-Pixelbild-Transform.png" alt="Developer Open Space" ></a>
             </li>
             <li>
+                <a href="https://spartakiade.org"><img src="images/Spartakiade_Logo.png" alt=""></a>
+            </li>
+            <!--li>
                 <a href="#"><img src="images/microsoft.png" alt=""></a>
             </li -->
         </ul>

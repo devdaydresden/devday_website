eeb853a8ee2e0c2bf85d05f58efad035f97d869f
Michael Strache <michael.strache@t-systems.com>
2016-02-04 18:25:08 +0100

    Updated Speaker Infos Updates Session Plan

--- contents/sessions.php
+++ contents/sessions.php
@@ -9,123 +9,124 @@
             Zeige Sessions
             <i class="fa fa-angle-down"></i>
         </h3>
+        
         <div class="collapse" id="sessionCollapse">
-          <table>
-            <thead>
-                <tr>
-                    <th style="min-width:135px">Zeit</th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>12:00 - 13:00</td>
-                    <td colspan="3" class="align-center">Anmeldung</td>
-                </tr>
-                <tr>
-                    <td>13:00 - 13:15</td>
-                    <td colspan="3" class="align-center">
-                        Eröffnung durch Prof. Frank Schönefeld
-                        <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>13:15 - 14:15</td>
-                    <td colspan="3" class="align-center">
-                        Keynote (Englisch): The Rationale for Continuous Delivery
-                        <p class="hint"><strong>Dave Farley</strong></p>
-                        <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>14:15 - 14:30</td>
-                    <td colspan="3" class="align-center">kurze Pause</td>
-                </tr>
-                <tr>
-                    <td>14:30 - 15:15</td>
-                    <td class="align-center">
-                        Session 1
-                        <p class="hint"><strong>TBA</strong></p>
-                        <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
-                    </td>
-                    <td class="align-center">
-                       Session 2<br>
-                       <p class="hint"><strong>TBA</strong></p>
-                       <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Gartensaal</p>
-                   </td>
-                   <td class="align-center">
-                    Session 3
-                    <p class="hint"><strong>TBA</strong></p>
-                    <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Festsaal</p>
-                </td>
-            </tr>
-            <tr>
-                <td>15:15 - 15:45</td>
-                <td colspan="3" class="align-center">Kaffeepause</td>
-            </tr>
-            <tr>
-                <td>15:45 - 16:30</td>
-                <td class="align-center">
-                    Session 4
-                    <p class="hint"><strong>TBA</strong></p>
-                    <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
-                </td>
-                <td class="align-center">
-                   Session 5
-                   <p class="hint"><strong>TBA</strong></p>
-                   <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Gartensaal</p>
-               </td>
-               <td class="align-center">
-                Session 6
-                <p class="hint"><strong>TBA</strong></p>
-                <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Festsaal</p>
-            </td>
-        </tr>
-        <tr>
-            <td>16:30 - 16:45</td>
-            <td colspan="3" class="align-center">kurze Pause</td>
-        </tr>
-        <tr>
-            <td>16:45 - 17:30</td>
-            <td class="align-center">
-                Session 7
-                <p class="hint"><strong>TBA</strong></p>
-                <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
-            </td>
-            <td class="align-center">
-               Session 8
-               <p class="hint"><strong>TBA</strong></p>
-               <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Gartensaal</p>
-           </td>
-           <td class="align-center">
-            Session 9
-            <p class="hint"><strong>TBA</strong></p>
-            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Festsaal</p>
-        </td>
-    </tr>
-    <tr>
-        <td>17:30 - 17:45</td>
-        <td colspan="3" class="align-center"> kurze Pause</td>
-    </tr>
-    <tr>
-        <td>17:45 - 18:45</td>
-        <td colspan="3" class="align-center">
-            Eine sprachneutrale Essenz der Microservices
-            <p class="hint"><strong>Adam Bien</strong></p>
-            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
-        </td>
-    </tr>
-    <tr>
-        <td>18:45 - 20:30</td>
-        <td colspan="3" class="align-center">Get together</td>
-    </tr>
-
-</tbody>
-</table>
-</div>
-</div>
+            <table>
+                <thead>
+                    <tr>
+                        <th style="min-width:135px">Zeit</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>12:00 - 13:00</td>
+                        <td colspan="3" class="align-center">Anmeldung</td>
+                    </tr>
+                    <tr>
+                        <td>13:00 - 13:15</td>
+                        <td colspan="3" class="align-center">
+                            Eröffnung durch Prof. Frank Schönefeld
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>13:15 - 14:15</td>
+                        <td colspan="3" class="align-center">
+                            Keynote (Englisch): The Rationale for Continuous Delivery
+                            <p class="hint"><strong>Dave Farley</strong></p>
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>14:15 - 14:30</td>
+                        <td colspan="3" class="align-center">kurze Pause</td>
+                    </tr>
+                    <tr>
+                        <td>14:30 - 15:15</td>
+                        <td class="align-center">
+                            DevOps - Microsoft Developer Divisions Weg ins nächste Agile Zeitalter
+                            <p class="hint"><strong>Artur Speth</strong></p>
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
+                        </td>
+                        <td class="align-center">
+                            Continuous Delivery - Aber Sicher?!
+                            <p class="hint"><strong>Jan Dittberner</strong></p>
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Gartensaal</p>
+                        </td>
+                        <td class="align-center">
+                            Lose gekoppelt wie nie: DI vs. IoC
+                            <p class="hint"><strong>Hendrik Lösch</strong></p>
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: St. Petersburg</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>15:15 - 15:45</td>
+                        <td colspan="3" class="align-center">Kaffeepause</td>
+                    </tr>
+                    <tr>
+                        <td>15:45 - 16:30</td>
+                        <td class="align-center">
+                            Acceptance Testing for Continuous Delivery
+                            <p class="hint"><strong>Dave Farley</strong></p>
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
+                        </td>
+                        <td class="align-center">
+                            MIXING UP REALITIES – Mit AR, VR, AV zu neuen Realitäten
+                            <p class="hint"><strong>Frank Lamack & Denis Loh</strong></p>
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Gartensaal</p>
+                       </td>
+                       <td class="align-center">
+                            Testautomatisierungsframework Xeta
+                            <p class="hint"><strong>Peter Lehmann</strong></p>
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: St. Petersburg</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>16:30 - 16:45</td>
+                        <td colspan="3" class="align-center">kurze Pause</td>
+                    </tr>
+                    <tr>
+                        <td>16:45 - 17:30</td>
+                        <td class="align-center">
+                            Sicherheit der Dinge – "Hacking im IoT"
+                            <p class="hint"><strong>Thomas Haase</strong></p>
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
+                        </td>
+                        <td class="align-center">
+                            Kleine Änderungen - enorme Wirkung: Datenbank-Coding für den Echtbetrieb
+                            <p class="hint"><strong>Carsten Czarski</strong></p>
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Gartensaal</p>
+                        </td>
+                        <td class="align-center">
+                            Cloud-Umgebungen mit Terraform verwalten
+                            <p class="hint"><strong>Sascha Askani</strong></p>
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: St. Petersburg</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>17:30 - 17:45</td>
+                        <td colspan="3" class="align-center"> kurze Pause</td>
+                    </tr>
+                    <tr>
+                        <td>17:45 - 18:45</td>
+                        <td colspan="3" class="align-center">
+                            Eine sprachneutrale Essenz der Microservices
+                            <p class="hint"><strong>Adam Bien</strong></p>
+                            <p class="hint italic"><i class="fa fa-arrow-circle-o-down"></i> Raum: Hamburg</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>18:45 - 20:30</td>
+                        <td colspan="3" class="align-center">Get together</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+    </div>
 
 </section>
 

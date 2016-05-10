d449aae422253ac5c5a2108d14b10d5a4b3774e8
Jan Dittberner <jan.dittberner@t-systems.com>
2016-05-10 21:21:14 +0200

    Repair user image and tweet template

--- public/contents/tweets/template/tweet-template.php
+++ public/contents/tweets/template/tweet-template.php
@@ -1,11 +1,9 @@
 <div class="item">
     <div>
-        <img src="<?=$tweetUserPic?>" alt="@<?=tweetUserName?>" class="left">
-        <h4><?=$tweetUserFullName?></h4>
-        <h5 class="light-blue">@<?=$tweetUserName?></h5>
+        <img src="<?= $tweetUserPic; ?>" alt="@<?= $tweetUserName; ?>" class="left">
+        <h4><?= $tweetUserFullName; ?></h4>
+        <h5 class="light-blue">@<?= $tweetUserName; ?></h5>
     </div>
     <div class="clearfix"></div>
-    <p>
-        <?=$tweetText?>
-    </p>
+    <p><?= $tweetText; ?></p>
 </div>
--- public/contents/tweets/tweet1.php
+++ public/contents/tweets/tweet1.php
@@ -1,5 +1,5 @@
 <?php
-    $tweetUserPic       = "https://pbs.twimg.com/profile_images/686663510156247040/POZNqhwx.png";
+    $tweetUserPic       = "https://pbs.twimg.com/profile_images/714469084944068610/6PKZOoeu_bigger.jpg";
     $tweetUserName      = "okrone68";
     $tweetUserFullName  = "Koboldland";
     $tweetText          = "<a href='https://twitter.com/hashtag/devdaydd?src=hash'>#devdaydd</a> Eine großartige Veranstaltung, mit interessanten Vorträgen und Inspirationen. Danke an den Veranstalter und die Dozenten";

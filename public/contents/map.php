d49c52025aad4399c6743183efcab97a4b4c8dc1
Jan Dittberner <jan.dittberner@t-systems.com>
2016-05-12 16:09:08 +0200

    Move texts to content snippets

--- public/contents/map.php
+++ public/contents/map.php
@@ -1,57 +1,47 @@
 <!-- BEGIN MAP -->
-
 <section class="map" id="anfahrt">
-
-    <div id="map"></div>
-
-
-    <div id="infobox1">
-        <p><strong><?=$mapShowAddress?></strong></p>
-    </div>
-    <div class="infobox-wrapper">
-        <div id="infobox">
-            <h2><?=$mapHeadline?></h2>
-            <article class="left first">
-                <h4><?=$mapHeadline2?></h4>
-                <h5><a href="https://www.google.com/maps/d/edit?mid=zBfktBmVT9XI.kY8LsPwquYbs" target="_blank"><?=$mapHeadline3?></a></h5>
-            </article>
-            <article class="left">
-                <ul>
-                    <li>
-                        <h6 class="normal-headline"><?=$mapAddressHeadline?></h6>
-                        <?=$mapAddress?>
-                        <div class="clearfix"></div>
-                    </li>
-                </ul>
-            </article>
+  <div id="map"></div>
+  <div id="infobox1">
+    <p><strong>Zeige Adresse</strong></p>
+  </div>
+  <div class="infobox-wrapper">
+    <div id="infobox">
+      <h2>Veranstaltungsort</h2>
+      <article class="left first">
+        <h4>Hier fand der DevDay 2016 statt</h4>
+        <h5><a href="https://www.google.com/maps/d/edit?mid=zBfktBmVT9XI.kY8LsPwquYbs" target="_blank">Veranstaltungsort in Google Maps anzeigen</a></h5>
+      </article>
+      <article class="left">
+        <ul>
+          <li>
+            <h6 class="normal-headline">Adresse</h6>
+            <p>Börse Dresden</p><p>Messering 3</p><p>01067 Dresden</p>
             <div class="clearfix"></div>
-
-        </div>
+          </li>
+        </ul>
+      </article>
+      <div class="clearfix"></div>
     </div>
-
-    <div class="show-mob">
-
-        <article class="left">
-            <h2><?=$mapHeadline?></h2>
-            <h4><?=$mapHeadline2?></h4>
-            <h5><a href="https://www.google.com/maps/d/edit?mid=zBfktBmVT9XI.kY8LsPwquYbs" target="_blank"><?=$mapHeadline3?></a></h5>
-        </article>
-        <article class="left">
-            <ul>
-                <li>
-                    <h6 class="normal-headline"><?=$mapAddressHeadline?></h6>
-                    <?=$mapAddress?>
-                    <div class="clearfix"></div>
-                </li>
-            </ul>
-        </article>
-        <div class="clearfix"></div>
-
-    </div>
-
+  </div>
+  <div class="show-mob">
+    <article class="left">
+      <h2>Veranstaltungsort</h2>
+      <h4>Hier fand der DevDay 2016 statt</h4>
+      <h5><a href="https://www.google.com/maps/d/edit?mid=zBfktBmVT9XI.kY8LsPwquYbs" target="_blank">Veranstaltungsort in Google Maps anzeigen</a></h5>
+    </article>
+    <article class="left">
+      <ul>
+        <li>
+          <h6 class="normal-headline">Adresse</h6>
+          <p>Börse Dresden</p><p>Messering 3</p><p>01067 Dresden</p>
+          <div class="clearfix"></div>
+        </li>
+      </ul>
+    </article>
+    <div class="clearfix"></div>
+  </div>
 </section>
-
-                <!-- END MAP -->
+<!-- END MAP -->
 <?php
 // vim: et sw=2 ts=2 ai si
 ?>

682db5e9150c4a12b2f3e39c5e6291800aae3591
Jan Dittberner <jan.dittberner@t-systems.com>
2016-08-02 17:06:28 +0200

    Implement form procession logic for talk submission

--- devday/talk/forms.py
+++ devday/talk/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from attendee.forms import AttendeeForm
 from devday.utils.forms import CombinedFormBase
@@ -12,6 +13,10 @@ class TalkForm(forms.models.ModelForm):
 
 
 class SpeakerForm(forms.models.ModelForm):
+    firstname = forms.fields.CharField(label=_("Firstname"), max_length=64)
+    lastname = forms.fields.CharField(label=_("Lastname"), max_length=64)
+    email = forms.fields.EmailField(label=_("Email"))
+
     class Meta:
         model = Speaker
         fields = ["portrait", "shortbio", "videopermission"]
--- devday/talk/views.py
+++ devday/talk/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import redirect
 from django.views.generic import FormView
+from django.contrib.auth import get_user_model
 
+from attendee.models import Attendee
 from talk.forms import CreateTalkWithSpeakerForm
+from talk.models import Speaker
+
+User = get_user_model()
 
 
 class CreateTalkWithSpeakerView(FormView):
@@ -9,5 +14,36 @@ class CreateTalkWithSpeakerView(FormView):
     form_class = CreateTalkWithSpeakerForm
 
     def form_valid(self, form):
-        # TODO: implement form processing
+        firstname = form.cleaned_data['firstname']
+        lastname = form.cleaned_data['lastname']
+        email = form.cleaned_data['email']
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            user = User.objects.create_user(firstname=firstname, lastname=lastname, email=email)
+            user.save()
+
+        try:
+            attendee = Attendee.objects.get(user=user)
+            attendee.shirt_size = form.attendeeform.instance.shirt_size
+        except Attendee.DoesNotExist:
+            attendee = form.attendeeform.instance
+            attendee.user = user
+        attendee.save()
+
+        try:
+            speaker = Speaker.objects.get(user=attendee)
+            speaker.portrait = form.speakerform.instance.portrait
+            speaker.shortbio = form.speakerform.instance.shortbio
+            speaker.videopermission = form.speakerform.instance.videopermission
+        except Speaker.DoesNotExist:
+            speaker = form.speakerform.instance
+            speaker.user = attendee
+        speaker.save()
+
+        talk = form.talkform.instance
+        talk.speaker = speaker
+        talk.save()
+
         return redirect(self.get_success_url())

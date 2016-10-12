9f9bd6d2dad0214e753b1bfb53f0858a75959e49
Jan Dittberner <jan.dittberner@t-systems.com>
2016-10-12 16:54:35 +0200

    Adapt to MMS environment, only build on changes

--- devday/deployment/build.sh
+++ devday/deployment/build.sh
@@ -7,10 +7,20 @@
 #
 set -ex
 cd $(dirname $0)
-docker pull centos:7
-docker build -t saec/devday_build .
+
 if [ -d result ]; then
+    if [ -f result/devday_hp_venv.tar.gz ]; then
+        if [ result/devday_hp_venv.tar.gz -nt ../requirements.txt ]; then
+            echo "venv tarball is already fresh"
+            exit 0
+        fi
+    fi
     rm -r result;
 fi
+
 mkdir result
+docker pull centos:7
+
+cp -a ../requirements.txt .
+docker build -t saec/devday_build .
 docker run --rm -v $(readlink -f result):/root/result saec/devday_build

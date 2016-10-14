e4335e409e5e0a57a94ef1530b7a13fd6dde2667
Jan Dittberner <jan.dittberner@t-systems.com>
2016-10-14 11:18:44 +0200

    Configure production logging

--- devday/devday/settings/prod.py
+++ devday/devday/settings/prod.py
@@ -11,3 +11,22 @@ MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
 ALLOWED_HOSTS = ['devday.de', 'www.devday.de', 'q4deumsy0dg.mms-at-work.de', 'localhost']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join('/home/devdayprod/devday.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}

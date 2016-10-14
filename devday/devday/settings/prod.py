e7fc4f5401be978132f93e27ce74e77d263effec
Jan Dittberner <jan.dittberner@t-systems.com>
2016-10-14 10:19:47 +0200

    Add q4deumsy0dg and localhost to ALLOWED_HOSTS setting

--- devday/devday/settings/prod.py
+++ devday/devday/settings/prod.py
@@ -5,4 +5,4 @@ Django production settings for devday.
 # noinspection PyUnresolvedReferences
 from .base import *
 
-ALLOWED_HOSTS = ['devday.de', 'www.devday.de']
+ALLOWED_HOSTS = ['devday.de', 'www.devday.de', 'q4deumsy0dg.mms-at-work.de', 'localhost']

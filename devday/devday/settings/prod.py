22c7da469648bc486009f9fd4b6b9045cf3dfe2d
Jan Dittberner <jan.dittberner@t-systems.com>
2016-10-14 10:57:43 +0200

    Change data directories for prod

--- devday/devday/settings/prod.py
+++ devday/devday/settings/prod.py
@@ -5,4 +5,9 @@ Django production settings for devday.
 # noinspection PyUnresolvedReferences
 from .base import *
 
+DATA_DIR = '/var/www/html/devday'
+
+MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
+STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+
 ALLOWED_HOSTS = ['devday.de', 'www.devday.de', 'q4deumsy0dg.mms-at-work.de', 'localhost']

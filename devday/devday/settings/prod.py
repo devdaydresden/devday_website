68610760cf16cb4f0dac21e770f1f2af160cd581
Jan Dittberner <jan.dittberner@t-systems.com>
2016-11-01 20:49:21 +0100

    Configure production logging

--- devday/devday/settings/prod.py
+++ devday/devday/settings/prod.py
@@ -10,20 +10,49 @@ DATA_DIR = '/var/www/html/devday'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
+ADMINS = [
+    ('Jan Dittberner', 'jan.dittberner@t-systems.com'),
+]
+
 ALLOWED_HOSTS = ['devday.de', 'www.devday.de', 'q4deumsy0dg.mms-at-work.de', 'localhost', 'devday-test.t-systems-mms.eu']
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)s %(message)s'
+        },
+        'simple': {
+            'format': '%(asctime)s %(levelname)s %(message)s'
+        }
+
+    },
     'handlers': {
         'file': {
             'level': 'INFO',
             'class': 'logging.FileHandler',
-            'filename': os.path.join('/home/devdayprod/devday.log'),
+            'filename': '/home/devdayprod/devday.log',
+            'formatter': 'simple',
         },
     },
     'loggers': {
         'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'attendee': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'devday': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'talk': {
             'handlers': ['file'],
             'level': 'INFO',
             'propagate': True,

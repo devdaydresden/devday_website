6623b6d9de4d18141a71c8e7ff3e6fe70cff553b
Jan Dittberner <jan.dittberner@t-systems.com>
2016-12-16 14:09:49 +0100

    Push test coverage for talk.models to 100%

--- devday/talk/tests/test_models.py
+++ devday/talk/tests/test_models.py
@@ -1,4 +1,7 @@
+import os
+
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TransactionTestCase
 
 from attendee.models import Attendee
@@ -16,6 +19,46 @@ class SpeakerTest(TransactionTestCase):
         speaker = Speaker.objects.create(
             user=self.attendee, videopermission=True, shirt_size=1)
         self.assertEqual("{}".format(speaker), "{}".format(self.attendee))
+
+    def test_create_thumbnail_png(self):
+        speaker = Speaker.objects.create(
+            user=self.attendee, videopermission=True, shirt_size=1)
+        speaker.portrait = SimpleUploadedFile(
+            name='mu_at_mil_house.png',
+            content=open(os.path.join(os.path.dirname(__file__), 'mu_at_mil_house.png'), 'rb').read(),
+            content_type='image/png')
+        speaker.create_thumbnail()
+        self.assertIsNotNone(speaker.thumbnail.name)
+
+    def test_create_thumbnail_jpg(self):
+        speaker = Speaker.objects.create(
+            user=self.attendee, videopermission=True, shirt_size=1)
+        speaker.portrait = SimpleUploadedFile(
+            name='mu_at_mil_house.jpg',
+            content=open(os.path.join(os.path.dirname(__file__), 'mu_at_mil_house.jpg'), 'rb').read(),
+            content_type='image/jpeg')
+        speaker.create_thumbnail()
+        self.assertIsNotNone(speaker.thumbnail.name)
+
+    def test_create_thumbnail_other(self):
+        speaker = Speaker.objects.create(
+            user=self.attendee, videopermission=True, shirt_size=1)
+        speaker.portrait = SimpleUploadedFile(
+            name='mu_at_mil_house.svg',
+            content=open(os.path.join(os.path.dirname(__file__), 'mu_at_mil_house.svg'), 'rb').read(),
+            content_type='application/xml+svg')
+        speaker.create_thumbnail()
+        self.assertIsNone(speaker.thumbnail.name)
+
+    def test_save_creates_thumbnail(self):
+        speaker = Speaker.objects.create(
+            user=self.attendee, videopermission=True, shirt_size=1)
+        speaker.portrait = SimpleUploadedFile(
+            name='mu_at_mil_house.jpg',
+            content=open(os.path.join(os.path.dirname(__file__), 'mu_at_mil_house.jpg'), 'rb').read(),
+            content_type='image/jpeg')
+        speaker.save()
+        self.assertIsNotNone(speaker.thumbnail.name)
 
 
 class TalkTest(TransactionTestCase):

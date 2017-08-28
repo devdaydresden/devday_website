9b6ea0c436177dc074bff4f28b61809497488a38
Jan Dittberner <jan.dittberner@t-systems.com>
2017-08-28 22:43:18 +0200

    Fix group permission initialization problem

--- devday/talk/apps.py
+++ devday/talk/apps.py
@@ -1,7 +1,24 @@
 from __future__ import unicode_literals
 
-from django.apps import AppConfig
+from django.apps import apps, AppConfig
+from django.db.models.signals import post_migrate
 
 
 class SessionsConfig(AppConfig):
     name = 'talk'
+
+    def ready(self):
+        post_migrate.connect(create_talk_committee, sender=self)
+
+
+def create_talk_committee(**kwargs):
+    Group = apps.get_model('auth', 'Group')
+    Permission = apps.get_model('auth', 'Permission')
+
+    group, created = Group.objects.get_or_create(name='talk_committee')
+
+    can_vote = Permission.objects.get(content_type__app_label='talk', codename='add_vote')
+    can_comment = Permission.objects.get(content_type__app_label='talk', codename='add_talkcomment')
+    group.permissions.set([can_comment, can_vote])
+
+    group.save()
--- devday/talk/migrations/0006_auto_20170104_1555.py
+++ devday/talk/migrations/0006_auto_20170104_1555.py
@@ -6,21 +6,6 @@ from django.contrib.auth.management import create_permissions
 from django.db import migrations, models
 
 
-def create_talk_committee(apps, schema_editor):
-    for app_config in apps.get_app_configs():
-        create_permissions(app_config, apps=apps, verbosity=0)
-
-    Group = apps.get_model('auth', 'Group')
-    Permission = apps.get_model('auth', 'Permission')
-
-    group, created = Group.objects.get_or_create(name='talk_committee')
-    if created:
-        can_vote = Permission.objects.get(content_type__app_label='talk', codename='add_vote')
-        can_comment = Permission.objects.get(content_type__app_label='talk', codename='add_talkcomment')
-        group.permissions.set([can_vote, can_comment])
-        group.save()
-
-
 class Migration(migrations.Migration):
     dependencies = [
         ('auth', '0002_alter_permission_name_max_length'),
@@ -34,5 +19,4 @@ class Migration(migrations.Migration):
             field=models.BooleanField(default=False,
                                       help_text='Indicates whether the comment is visible to the speaker.'),
         ),
-        migrations.RunPython(create_talk_committee)
     ]

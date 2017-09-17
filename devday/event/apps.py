885f606c2b8c95bdedb4ac3aa3be826cc8f4ff1e
Jan Dittberner <jan.dittberner@t-systems.com>
2017-09-17 12:29:07 +0200

    SAEC-19 Add german translation
    Signed-off-by: Jan Dittberner <jan.dittberner@t-systems.com>

--- devday/event/apps.py
+++ devday/event/apps.py
@@ -2,12 +2,15 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
+from django.utils.translation import ugettext_lazy as _
 
 from event.management import create_default_event
 
 
 class EventsConfig(AppConfig):
     name = 'event'
+
+    verbose_name = _("Events")
 
     def ready(self):
         post_migrate.connect(create_default_event, sender=self)

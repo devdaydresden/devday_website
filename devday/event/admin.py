46cae85c273bfa10124487691004c8e829d0dff2
Stefan Bethke <stefan.bethke@t-systems.com>
2018-01-12 13:56:31 +0100

    SAEC-90 show registration count in event admin page

--- devday/event/admin.py
+++ devday/event/admin.py
@@ -5,7 +5,7 @@ from .models import Event
 
 class EventAdmin(admin.ModelAdmin):
     model = Event
-
+    list_display = ('title', 'registration_count')
     prepopulated_fields = {'slug': ("title",)}
 
 

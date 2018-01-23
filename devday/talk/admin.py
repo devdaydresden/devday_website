f0a7039d3682739d95604311f19b4d3a2676fee7
Stefan Bethke <stefan.bethke@t-systems.com>
2018-01-23 17:30:17 +0100

    Suchfelder für Speaker.

--- devday/talk/admin.py
+++ devday/talk/admin.py
@@ -3,6 +3,19 @@ from django.contrib import admin
 from .models import Speaker, Talk, Track, Room, TimeSlot, TalkSlot, TalkMedia
 
 
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = ['fullname', 'videopermission', 'shirt_size', 'event']
+    search_fields = ['user__user__first_name', 'user__user__last_name', 'user__user__email']
+    list_filter = ['user__event']
+
+    def event(self, obj):
+        return obj.user.event
+
+    def fullname(self, obj):
+        obj = obj.user.user
+        return u"{} {} <{}>".format(obj.first_name, obj.last_name, obj.email)
+
+
 class TalkMediaInline(admin.StackedInline):
     model = TalkMedia
 
@@ -26,7 +39,7 @@ class TimeSlotAdmin(admin.ModelAdmin):
     list_display = ['name', 'event' ]
 
 
-admin.site.register(Speaker)
+admin.site.register(Speaker, SpeakerAdmin)
 admin.site.register(Talk, TalkAdmin)
 admin.site.register(Track)
 admin.site.register(Room)

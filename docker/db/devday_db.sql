fc88270eb2879dbad0c1a0a97cc15a68f54c2b45
Jan Dittberner <jan.dittberner@t-systems.com>
2018-09-04 13:57:41 +0200

    Fix WSGI setup

--- docker/db/devday_db.sql
+++ docker/db/devday_db.sql
@@ -5,5 +5,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE USER devday WITH PASSWORD 'devday';
 CREATE DATABASE devday ENCODING 'UTF-8' TEMPLATE 'template0';
-GRANT CREATE, USAGE ON DATABASE devday TO devday;
+GRANT CREATE, CONNECT ON DATABASE devday TO devday;
 EOSQL

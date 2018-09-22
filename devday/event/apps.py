3d41fcd1788375a7d9f588f8b05eef8c56f8f0b6
Jan Dittberner <jan.dittberner@t-systems.com>
2018-09-23 00:38:25 +0200

    SECO-61 Remove broken import

--- devday/event/apps.py
+++ devday/event/apps.py
@@ -4,13 +4,8 @@ from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 from django.utils.translation import ugettext_lazy as _
 
-from event.management import create_default_event
-
 
 class EventsConfig(AppConfig):
     name = 'event'
 
     verbose_name = _("Events")
-
-    def ready(self):
-        post_migrate.connect(create_default_event, sender=self)

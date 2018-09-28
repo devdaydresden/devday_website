6b0c286893426fd2e2b8959fccc81ed51eef2435
Stefan Bethke <stefan.bethke@t-systems.com>
2018-09-28 11:25:51 +0200

    Cleanup #noissue

--- dev_settings.bat
+++
@@ -1,7 +0,0 @@
-set DEVDAY_PG_DBNAME=devday
-set DEVDAY_PG_HOST=192.168.199.200
-set DEVDAY_PG_PORT=5432
-set DEVDAY_PG_USER=devday
-set DEVDAY_PG_PASSWORD=secret
-set DEVDAY_SECRET=r+-e#7^6mwh_44f6@nqma^3w!y6(ob3c304mc27#0ccu6^6k%0
-set DJANGO_SETTINGS_MODULE=devday.settings.dev

456559bccad8c3a28b42ba54ae531f9c385860c2
Jan Dittberner <jan@dittberner.info>
2018-10-01 20:50:11 +0200

    Fix Python 3 incompatibility of useremails command

--- devday/devday/management/commands/useremails.py
+++ devday/devday/management/commands/useremails.py
@@ -1,21 +1,14 @@
-import json
-from datetime import datetime, timedelta
-from email.utils import parsedate_tz
-
-import pytz
-import requests
-from django.conf import settings
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.management import BaseCommand
-from django.core.management import CommandError
-from django.db.transaction import atomic
 
 from attendee.models import DevDayUser
 
+
 class Command(BaseCommand):
-    help = 'Print out a list of all users that have agreed to be contacted by the team'
+    help = ('Print out a list of all users that have agreed to be contacted by'
+            ' the team')
 
     def handle(self, *args, **options):
-        users = DevDayUser.objects.all().filter(contact_permission_date__isnull=False).order_by('email')
+        users = DevDayUser.objects.all().filter(
+            contact_permission_date__isnull=False).order_by('email')
         for user in users:
-            print "{}".format(user.email)
+            self.stdout.write(user.email)

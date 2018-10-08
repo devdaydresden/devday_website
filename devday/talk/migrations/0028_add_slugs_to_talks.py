7dbef22b0fc9effc75afb6ef7fb6421b3e311b23
Jan Dittberner <jan.dittberner@t-systems.com>
2018-10-08 17:24:09 +0200

    Fix data migrations to work on production
    - adapt length of Talk.slug to match Talk.title
    - alter existing migration 0028 to work on databases that did not have
      the slug field
    - add new migration 0029 that works on databases where 0028 has already
      been run
    - split 0028 to avoid running into DatabaseError due to modifications of
      a table with data in the same transaction as the table creation
    - adapt run.sh restore to the same media tar.gz format that is produced
      by ./prod.sh backup
    
    Fixes #32

--- devday/talk/migrations/0028_add_slugs_to_talks.py
+++ devday/talk/migrations/0028_add_slugs_to_talks.py
@@ -3,18 +3,9 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-from django.utils.text import slugify
-
-
-def create_default_slug(apps, schema_manager):
-    Talk = apps.get_model('talk', 'Talk')
-    for talk in Talk.objects.all():
-        talk.slug = slugify(talk.title)
-        talk.save()
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('talk', '0027_talkformats'),
     ]
@@ -23,8 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='talk',
             name='slug',
-            field=models.SlugField(default='', verbose_name='Slug'),
+            field=models.SlugField(max_length=255, default='', verbose_name='Slug'),
             preserve_default=False,
         ),
-        migrations.RunPython(create_default_slug)
     ]

0f61b2c723c384575eafcb7b9d42c9ab7ad0e891
Jan Dittberner <jan@dittberner.info>
2018-10-10 22:29:29 +0200

    Make speaker admin views usable

--- devday/speaker/admin.py
+++ devday/speaker/admin.py
@@ -5,9 +5,16 @@ from .models import PublishedSpeaker, Speaker
 
 @admin.register(Speaker)
 class SpeakerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'twitter_handle', 'position', 'organization')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('video_permission',)
+    search_fields = ('name', 'twitter_handle', 'position', 'organization')
 
 
 @admin.register(PublishedSpeaker)
 class PublishedSpeakerAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'name', 'event', 'twitter_handle', 'position', 'organization')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('video_permission', 'event')
+    search_fields = ('name', 'twitter_handle', 'position', 'organization')

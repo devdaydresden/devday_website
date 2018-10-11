a987ddfb256ef89212fc952dbc80ef05119509ac
Stefan Bethke <stefan.bethke@t-systems.com>
2018-10-11 15:34:09 +0200

    Correct test setup

--- devday/event/tests/test_cms_menus.py
+++ devday/event/tests/test_cms_menus.py
@@ -9,22 +9,8 @@ from devday.utils.devdata import DevData
 from event.models import Event
 
 
-# FIXME apparently there's a bug in django-cms 3.5.2: Django's
-# user.is_authenticated has been a property since 1.10, but django-cms
-# treats it as a callable.
-#   File "/usr/local/lib/python3.6/site-packages/menus/menu_pool.py", line 243,
-#       in get_renderer
-#     return MenuRenderer(pool=self, request=request)
-#   File "/usr/local/lib/python3.6/site-packages/menus/menu_pool.py", line 107,
-#       in __init__
-#     self.draft_mode_active = use_draft(request)
-#   File "/usr/local/lib/python3.6/site-packages/cms/utils/moderator.py",
-#       line 5, in use_draft
-#     is_staff = (request.user.is_authenticated() and request.user.is_staff)
-# TypeError: 'property' object is not callable
-
 def get_register_menu(register_menu, request):
-        request.user = AnonymousUser
+        request.user = AnonymousUser()
         with patch('menus.menu_pool.use_draft', return_value=False):
             renderer = menu_pool.get_renderer(request)
         return menu_pool.menus[register_menu](renderer)

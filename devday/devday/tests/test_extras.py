5d75c864da9790e3f6f574bd24af54e416f7c812
Jan Dittberner <jan.dittberner@t-systems.com>
2018-10-20 12:23:54 +0200

    Improve ValidatedImageField
    Test ValidatedImageField via the real model API using an InMemoryStorage
    implementation. Convert the class method that stopped custom extensions
    from working into an instance method.

--- devday/devday/tests/test_extras.py
+++ devday/devday/tests/test_extras.py
@@ -2,7 +2,9 @@ from io import BytesIO
 
 from django.core.exceptions import ValidationError
 from django.core.files.uploadedfile import InMemoryUploadedFile
-from django.test import TestCase
+from django.db.models import Model
+from django.test import TestCase, override_settings
+from django.utils.translation import ugettext as _
 
 from devday.extras import ValidatedImageField
 
@@ -13,33 +15,52 @@ small_gif = (
 )
 
 
+def get_in_memory_gif_file(data):
+    return InMemoryUploadedFile(
+        BytesIO(data),
+        field_name='image',
+        name='an_image.png',
+        content_type='image/png',
+        size=len(data),
+        charset='utf-8',
+    )
+
+
+@override_settings(DEFAULT_FILE_STORAGE='inmemorystorage.InMemoryStorage')
 class ValidatedImageFieldTest(TestCase):
-    '''
+    """
     Explicitly test the custom clean() call in our ImageField.  Although the
     code should be exercised during Form tests, having an explicit test here
     should make finding problems easier.
-    '''
-
-    @staticmethod
-    def get_uploaded_file(data):
-        return InMemoryUploadedFile(
-            BytesIO(data),
-            field_name='image',
-            name='an_image.png',
-            content_type='image/png',
-            size=len(data),
-            charset='utf-8',
-        )
+    """
+    def setUp(self):
+        self.gif_file = get_in_memory_gif_file(small_gif)
 
     def test_valid(self):
-        name = ValidatedImageField.name_image_by_contents(
-            self.get_uploaded_file(small_gif))
+        class MockModelWithImageValid(Model):
+            image = ValidatedImageField()
+        instance = MockModelWithImageValid()
+        instance.image.save(self.gif_file.name, self.gif_file, save=False)
+        instance.full_clean()
         self.assertEquals(
-            name,
+            instance.image.name,
             'c7fbc3e8d889e42601d1dfa019f4678d67e2383a97a1940761d105ee.gif')
 
     def test_invalid(self):
+        class MockModelWithImageInvalid(Model):
+            image = ValidatedImageField()
+        instance = MockModelWithImageInvalid()
+        instance.image.save(
+            'invalid_image.jpg', BytesIO(b'not an image'), save=False)
         with self.assertRaises(ValidationError):
-            n = ValidatedImageField.name_image_by_contents(
-                self.get_uploaded_file(b'not an image'))
-            print('#### {} ####'.format(n))
+            instance.full_clean()
+
+    def test_custom_extensions_value(self):
+        class MockModelWithImageCustomExtensions(Model):
+            image = ValidatedImageField(extensions={'image/jpeg': '.jpg'})
+        instance = MockModelWithImageCustomExtensions()
+        instance.image.save(self.gif_file.name, self.gif_file, save=False)
+        with self.assertRaisesMessage(
+                ValidationError,
+                _('Unsupported image file format {}').format('image/gif')):
+            instance.full_clean()

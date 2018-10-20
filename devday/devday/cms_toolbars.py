8a00ea8ef7684bb987659e17638472601da476b9
Jan Dittberner <jan.dittberner@t-systems.com>
2018-10-20 12:26:16 +0200

    Remove unreachable condition

--- devday/devday/cms_toolbars.py
+++ devday/devday/cms_toolbars.py
@@ -7,11 +7,10 @@ from django.utils.translation import ugettext_lazy as _
 @toolbar_pool.register
 class DevDayToolbar(CMSToolbar):
     def populate(self):
-        if not self.is_current_app:
-            menu = self.toolbar.get_or_create_menu('devday', _('Dev Day'))
-            url = reverse('admin_csv_inactive')
-            menu.add_link_item(_('Inactive users'), url=url)
-            url = reverse('admin_csv_maycontact')
-            menu.add_link_item(_('Users we may contact'), url=url)
-            url = reverse('admin_csv_attendees')
-            menu.add_link_item(_("Current event attendees"), url=url)
+        menu = self.toolbar.get_or_create_menu('devday', _('Dev Day'))
+        url = reverse('admin_csv_inactive')
+        menu.add_link_item(_('Inactive users'), url=url)
+        url = reverse('admin_csv_maycontact')
+        menu.add_link_item(_('Users we may contact'), url=url)
+        url = reverse('admin_csv_attendees')
+        menu.add_link_item(_("Current event attendees"), url=url)

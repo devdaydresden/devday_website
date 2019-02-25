b3e017b465832b2707e7c12e85466bb62d954e51
Jan Dittberner <jan.dittberner@t-systems.com>
2019-02-25 14:45:14 +0100

    Implement talk publication action
    This commit adds a new admin action to publish a list of sessions from
    within the admin site.

--- devday/talk/admin.py
+++ devday/talk/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _, ngettext_lazy
 
 from talk.forms import SessionReservationForm
 from .models import (Room, Talk, TalkFormat, TalkMedia, TalkSlot, TimeSlot,
@@ -34,10 +36,37 @@ class TalkAdmin(admin.ModelAdmin):
         TalkSlotInline,
     ]
     ordering = ['title']
-    list_select_related = ['draft_speaker', 'published_speaker', 'event',
-                           'draft_speaker__user', 'track']
+    list_select_related = [
+        'draft_speaker', 'event', 'track', 'track__event']
     filter_horizontal = ('talkformat',)
     prepopulated_fields = {'slug': ("title",)}
+    actions = ['publish_talks']
+
+    def publish_talks(self, request, queryset):
+        if 'apply' in request.POST:
+            published = 0
+            for talk in queryset.all():
+                track_field_name = 'selected_track-{}'.format(talk.id)
+                if request.POST[track_field_name]:
+                    track = Track.objects.get(
+                        id=int(request.POST[track_field_name]))
+                    talk.publish(track)
+                    published += 1
+            self.message_user(
+                request,
+                ngettext_lazy(
+                    'One Session has been published.',
+                    '%(count)d sessions have been published.',
+                    published
+                ) % {'count': published})
+            return HttpResponseRedirect(request.get_full_path())
+
+        return render(request, 'talk/admin/publish_talks.html', context={
+            'talks': queryset,
+            'tracks': Track.objects.order_by('name')
+        })
+
+    publish_talks.short_description = _('Publish selected sessions')
 
 
 @admin.register(TalkSlot)

53457b4e803b0eb9b5145ce9572ab60ecdbd5c39
Jan Dittberner <jan.dittberner@t-systems.com>
2019-04-11 14:32:36 +0200

    Improve attendee admin performance
    This commit adds prefetching of related items for list display.

--- devday/attendee/admin.py
+++ devday/attendee/admin.py
@@ -2,47 +2,57 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext_lazy as _
 
-from .forms import (AttendeeInlineForm, DevDayUserChangeForm,
-                    DevDayUserCreationForm)
+from .forms import AttendeeInlineForm, DevDayUserChangeForm, DevDayUserCreationForm
 from .models import Attendee, DevDayUser
 
 
 @admin.register(Attendee)
 class AttendeeAdmin(admin.ModelAdmin):
-    list_display = ('user', 'event')
-    fields = ('user', 'source', 'event', 'checkin_code', 'checked_in')
-    list_filter = ('event',)
-    search_fields = ('user__email',)
-    ordering = ('event__title', 'user__email')
+    list_display = ("user", "event")
+    fields = ("user", "source", "event", "checkin_code", "checked_in")
+    list_filter = ("event",)
+    search_fields = ("user__email",)
+    ordering = ("event__title", "user__email")
 
 
 class AttendeeInline(admin.TabularInline):
     model = Attendee
-    fields = (('event', 'source'),)
-    ordering = ('event__title',)
+    fields = (("event", "source"),)
+    list_select_related = ("event",)
+    ordering = ("event__title",)
     extra = 0
     form = AttendeeInlineForm
 
 
 @admin.register(DevDayUser)
 class DevDayUserAdmin(UserAdmin):
-    list_display = ('email', 'is_staff', 'speaker')
-    search_fields = ('email', 'speaker__name')
-    list_filter = ('attendees__event', 'is_active', 'is_staff')
+    list_display = ("email", "is_staff", "speaker")
+    search_fields = ("email", "speaker__name")
+    list_filter = ("attendees__event", "is_active", "is_staff")
+    list_select_related = ("speaker",)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined',
-                                           'contact_permission_date')}),
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            _("Important dates"),
+            {"fields": ("last_login", "date_joined", "contact_permission_date")},
+        ),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
-        }),
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
     )
-    ordering = ('email',)
+    ordering = ("email",)
     add_form = DevDayUserCreationForm
     form = DevDayUserChangeForm
     inlines = (AttendeeInline,)

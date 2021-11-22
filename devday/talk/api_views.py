44ca247190f2491e80072c867dac6f78214b148d
Jens Lauterbach <jenslauterbach@icloud.com>
2021-11-22 23:34:02 +0100

    Add Speaker Information to Events API
    Instead of just containing a list of speaker names, after this patch is
    applied, the API will contain a list of objects. Object object per
    (published) speaker. The object will contain a URL to the speakers
    details (in the API), the name of the speaker and image URL of the
    speakers thumbnail.

--- devday/talk/api_views.py
+++ devday/talk/api_views.py
@@ -1,6 +1,29 @@
 from rest_framework import serializers, viewsets
 from rest_framework.relations import StringRelatedField
+
+from speaker.models import Speaker
 from talk.models import Talk
+
+
+class NestedSpeakersSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(source="thumbnail")
+
+    class Meta:
+        model = Speaker
+        fields = ["url", "name", "image"]
+        lookup_field = "slug"
+        extra_kwargs = {
+            "url": {'lookup_field': 'slug'}
+        }
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        # TODO Jens: This probably can be solved more elegantly with a mixin.
+        if not representation["image"]:
+            representation["image"] = ""
+
+        return representation
 
 
 class SessionSerializer(serializers.HyperlinkedModelSerializer):
@@ -9,11 +32,11 @@ class SessionSerializer(serializers.HyperlinkedModelSerializer):
 
     # TODO Jens: Replace 'event' and 'speakers' with proper nested relationships once their endpoints are done.
     event = StringRelatedField()
-    published_speakers = StringRelatedField(many=True)
+    speakers = NestedSpeakersSerializer(source="published_speakers", many=True, read_only=True)
 
     class Meta:
         model = Talk
-        fields = ["id", "url", "title", "description", "published_speakers", "event"]
+        fields = ["id", "url", "title", "description", "speakers", "event"]
         lookup_field = "slug"
         extra_kwargs = {
             "url": {'lookup_field': 'slug'}

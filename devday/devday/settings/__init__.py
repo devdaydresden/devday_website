893927a315322a92d18c97a236f302926ea154f8
Jan Dittberner <jan.dittberner@t-systems.com>
2022-01-28 12:05:21 +0100

    Add password validators
    This commits adds a list of password validators to avoid very weak
    passwords.

--- devday/devday/settings/__init__.py
+++ devday/devday/settings/__init__.py
@@ -109,6 +109,23 @@ ALLOWED_HOSTS = get_setting(
     "ALLOWED_HOSTS", split_list, default_value=["0.0.0.0", "127.0.0.1", "localhost"]
 )
 AUTH_USER_MODEL = "attendee.DevDayUser"
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 8,
+        },
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 

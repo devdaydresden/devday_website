afa5e300b8ec420da323f076627a03cbb3605c7f
Jan Dittberner <jan.dittberner@t-systems.com>
2022-05-20 12:14:45 +0200

    Apply Webstorm/PyCharm formatting

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,27 +1,27 @@
-const { series, src, dest } = require('gulp');
-const autoprefixer          = require('gulp-autoprefixer');
-const concat                = require('gulp-concat');
-const cssnano               = require('gulp-cssnano');
-const gulpif                = require('gulp-if');
-const header                = require('gulp-header');
-const plumber               = require('gulp-plumber');
-const sass                  = require('gulp-sass')(require('sass'));
-const sourcemaps            = require('gulp-sourcemaps');
-const uglify                = require('gulp-uglify');
-const fs                    = require('fs');
-const minimist              = require('minimist');
+const {series, src, dest} = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const cssnano = require('gulp-cssnano');
+const gulp_if = require('gulp-if');
+const header = require('gulp-header');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const fs = require('fs');
+const minimist = require('minimist');
 
-var config                  = require('./gulp/config');
-var options                 = minimist(process.argv.slice(2), config.environment);
+const config = require('./gulp/config');
+const options = minimist(process.argv.slice(2), config.environment);
 
-function build_js(callback) {
-    var result = src(config.js.src)
-        .pipe(gulpif(options.env === config.environment.production, sourcemaps.init({
+function build_js() {
+    const result = src(config.js.src)
+        .pipe(gulp_if(options.env === config.environment.production, sourcemaps.init({
             loadMaps: true
         }))) // init source maps in production
         .pipe(concat(config.js.filename))
-        .pipe(gulpif(options.env === config.environment.production, uglify())) // only minify in production
-        .pipe(gulpif(options.env === config.environment.production, sourcemaps.write('.'))) //write sourcemap in production
+        .pipe(gulp_if(options.env === config.environment.production, uglify())) // only minify in production
+        .pipe(gulp_if(options.env === config.environment.production, sourcemaps.write('.'))) //write sourcemap in production
         .pipe(dest(config.js.dest));
 
     if (fs.existsSync(config.js.dest + "/" + config.js.filename) && !fs.existsSync(config.js.dest + "/" + config.js.minFilename)) {
@@ -37,12 +37,12 @@ function build_js(callback) {
 }
 
 function build_jquery() {
-   return src(config.jquery.src).pipe(dest(config.jquery.dest));
+    return src(config.jquery.src).pipe(dest(config.jquery.dest));
 }
 
 // copy images
-function build_images(callback) {
-   return src(config.images.src).pipe(dest(config.images.dest));
+function build_images() {
+    return src(config.images.src).pipe(dest(config.images.dest));
 }
 
 function build_fonts() {
@@ -51,15 +51,15 @@ function build_fonts() {
 
 function build_sass() {
     return src(config.sass.src)
-        .pipe(gulpif(options.env === config.environment.production, sourcemaps.init({
+        .pipe(gulp_if(options.env === config.environment.production, sourcemaps.init({
             loadMaps: true
         }))) // init source maps in production
         .pipe(plumber())
         .pipe(sass())
         .pipe(header(config.strings.banner))
         .pipe(autoprefixer(config.sass.options.autoprefixer))
-        .pipe(gulpif(options.env === config.environment.production, cssnano())) // only minify in production
-        .pipe(gulpif(options.env === config.environment.production, sourcemaps.write('.'))) //write sourcemap in production
+        .pipe(gulp_if(options.env === config.environment.production, cssnano())) // only minify in production
+        .pipe(gulp_if(options.env === config.environment.production, sourcemaps.write('.'))) //write sourcemap in production
         .pipe(dest(config.sass.dest));
 }
 

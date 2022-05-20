95bcad01718ddabcdb891c55bd77fb778e03334d
Jan Dittberner <jan.dittberner@t-systems.com>
2022-05-20 10:53:28 +0200

    Remove unused task files

--- frontend/gulp/tasks/development/fonts.js
+++
@@ -1,7 +0,0 @@
-var gulp            = require('gulp');
-var config          = require('../../config');
-
-function build_fonts() {
-    return gulp.src(config.fonts.src)
-        .pipe(gulp.dest(config.fonts.dest));
-}
--- frontend/gulp/tasks/development/jquery.js
+++
@@ -1,8 +0,0 @@
-/*
-var { src, dest } = require('gulp');
-var config        = require('../config');
-
-function build_jquery() {
-   return src(config.jquery.src).pipe(dest(config.jquery.dest));
-}
-*/

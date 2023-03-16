3721fc32a6b7432c4d9f01e9937e17eda7c7d472
Jan Dittberner <jan.dittberner@telekom.de>
2023-03-16 12:07:09 +0100

    Remove proxy settings
    Where we go, we need no proxy settings :-)

--- frontend/docker/build.sh
+++ frontend/docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build --pull --build-arg "http_proxy=${http_proxy}" --build-arg "no_proxy=${no_proxy}" $(dirname $0) -t devday-website-frontend
+docker build --pull $(dirname $0) -t devday-website-frontend
